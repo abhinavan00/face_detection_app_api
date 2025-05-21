d5eae1ef29355b20954e225db23e223fa8a17378
Abhinav Anand <abhinavanand@Mac.lan>
2025-05-21 20:37:11 +0530

    adding cors

--- server.js
+++ server.js
@@ -20,17 +20,7 @@ const db = knex({
   },
 });
 
-// First, let's make your CORS configuration more flexible
-const corsOptions = {
-  origin: ['https://face-detection-app-wenk.onrender.com', process.env.FRONTEND_URL || 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions));
-app.use(app.use(cors({ origin: '*' })));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
